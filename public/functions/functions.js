222454a6db1a46db76bae487a7ef44465fc0bbed
mike-o31 <newyorkerboy31@yahoo.com>
2022-01-20 08:41:16 -0500

    Removed deprecated moment.js and replaced with luxon.js for date and time component

--- public/functions/functions.js
+++ public/functions/functions.js
@@ -1,34 +1,42 @@
-const moment = require('moment')
+const { DateTime } = require('luxon');
 
-const users = []
+const users = [];
 
 const messageFormat = (name, message, room) => {
-   return {
-       name: name,
-       message: message,
-       time: moment().format('MM/DD/YYYY, h:mm A'),
-       room: room
-   }
-}
+	const dt = DateTime.now();
+
+	return {
+		name: name,
+		message: message,
+		time: dt.toLocaleString(DateTime.DATETIME_SHORT),
+		room: room,
+	};
+};
 
 const userJoining = (id, name, room) => {
-    const user = { id, name, room }
+	const user = { id, name, room };
 
-    users.push(user)
+	users.push(user);
 
-    return user
-}
+	return user;
+};
 
-const getCurrentUser = (id) => users.find((user) => user.id === id)
+const getCurrentUser = (id) => users.find((user) => user.id === id);
 
 const userLeaving = (id) => {
-    const userIndex = users.findIndex((user) => user.id === id)
+	const userIndex = users.findIndex((user) => user.id === id);
 
-    if (userIndex !== -1) {
-        return users.splice(userIndex, 1)[0]
-    }
-}
+	if (userIndex !== -1) {
+		return users.splice(userIndex, 1)[0];
+	}
+};
 
-const getUsersInRoom = (room) => users.filter((user) => user.room === room)
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-module.exports = {  messageFormat, userJoining, getCurrentUser, userLeaving, getUsersInRoom }
+module.exports = {
+	messageFormat,
+	userJoining,
+	getCurrentUser,
+	userLeaving,
+	getUsersInRoom,
+};
